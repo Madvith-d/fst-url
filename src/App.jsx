05fa75ae9024c6be3b02baef0123e71d26914b79
madvith-d <madroux930@gmail.com>
2025-08-16 12:10:22 +0530

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,40 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter , Routes , Route ,Navigate, Router} from 'react-router'
+import { supabase } from './Supabase/supabaseClient'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import landing from './pages/landing'
+import login from './pages/login'
+import signup from './pages/signup'
+import home from './pages/home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+
+    supabase.auth.getUser().then(({ data: { user } }) => {
+      setUser(user);
+    });
+    const { data: { listener } } = supabase.auth.onAuthStateChange((_, session) => {
+      setUser(session?.user || null);
+    });
+
+    return () => {
+      listener.subscription.unsubscribe();
+    };
+  }, []);
 
   return (
     <>
-      
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/landing" />} />
+      </Routes>
+    </Router>
+     
     </>
   )
 }
